30b7c3cae09f11de2d9f6e056e5f37418e014a01
Shine145-Git <shineoneestate@gmail.com>
2025-07-11 17:15:02 +0530

    Update post.model.js

--- Server/models/post.model.js
+++ Server/models/post.model.js
@@ -15,6 +15,11 @@ const PostSchema = new mongoose.Schema({
     type: Number,
     required: true
   },
+  type: {
+  type: String,
+  enum: ['plot', 'flat'], // restricts values to only these two
+  required: true
+},
   area: {
     type: Number,
     required: true
